bd3c953f01fc591d16daf77921e58d697db1a50b
ln-dev7 <leonelngoya@gmail.com>
2022-01-04 16:00:52 +0100

    Mode responsive tablette

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { CrewComponent } from "./crew/crew.component";
+import { DestinationComponent } from "./destination/destination.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { TechnologyComponent } from "./technology/technology.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent
+  },
+  {
+    path: 'destination',
+    component: DestinationComponent
+  },
+  {
+    path: 'crew',
+    component: CrewComponent
+  },
+  {
+    path: 'technology',
+    component: TechnologyComponent
   }
 ];
 
